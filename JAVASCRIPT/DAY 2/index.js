47a057cdff60e61aed508f27446d50b57dc66979
Aarti kumari <161413939+Artikumari005@users.noreply.github.com>
2025-04-19 18:31:32 +0530

    Add files via upload

---
+++ JAVASCRIPT/DAY 2/index.js
@@ -0,0 +1,18 @@
+//arithematic operators
+
+//let a=45;
+//let b=5;
+//console.log(a++);
+//console.log(a);
+console.log(false||5||45||34);
+console.log(~(0));
+console.log(5<<1);
+console.log(5>>1)
+
+let age=30;
+if(age>20){
+    console.log('vote');
+}
+else{
+    console.log('not vote');
+}
