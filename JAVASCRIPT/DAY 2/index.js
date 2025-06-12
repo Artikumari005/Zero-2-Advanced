f9761359c5d95d16609817bddd333d3fd51d6a52
Aarti kumari <161413939+Artikumari005@users.noreply.github.com>
2025-06-12 20:21:17 +0530

    Add files via upload

--- JAVASCRIPT/DAY 2/index.js
+++ JAVASCRIPT/DAY 2/index.js
@@ -16,3 +16,17 @@ if(age>20){
 else{
     console.log('not vote');
 }
+
+let num=3;
+switch(num){
+    case 1:console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    case 4:console.log('D');
+    break;
+    default: console.log('F');
+}
+
---
+++ JAVASCRIPT/Day 4/index.js
@@ -0,0 +1,61 @@
+//function definition
+// function myName(){
+   // console.log("zennie");
+//}
+//function call
+// myName();
+
+
+//function counting(){
+//    for(let i=0;i<6;i++){
+//        console.log(i);
+//    }
+//}
+//counting();
+
+
+//function counting(num){
+  //  console.log("print number", num);
+//}
+//counting(6);//argument for function
+
+//function getAverage(num1,num2){
+  //  let avg=(num1+num2)/2;
+    //console.log("average:",avg);
+//}
+//getAverage(4,7);
+
+
+//return function  //
+//function getSum(a,b, c){
+   // let sum=a+b+c;
+   // return sum;
+//}
+//let ans=getSum(1,2,3);
+//console.log("printing sum:",ans);
+
+//function getName(firstName, lastName){
+//    let fullName= firstName+lastName;
+//    return fullName;
+//    //UNREACHABLE STATEMENTS
+//}
+//let fullName=getName("love" ,"babbar");
+//console.log("full name is:", fullName);
+
+//function getMultiplictaion(a,b){
+//    return a*b;
+//}
+//let ans=getMultiplictaion(8,4);
+//console.log(ans);
+
+//let getMultiplictaion= function(a,b){
+//    return a*b;
+//}
+//let ans=getMultiplictaion(76,3);
+//console.log(ans);
+
+let getMultiplictaion= (a,b)=>{
+   return a*b;
+}
+let ans=getMultiplictaion(76,3);
+console.log(ans);
